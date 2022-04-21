a4aa91da5e08e017ddf343bf97d4e7a91a72f532
Abylai <aba.omar@mail.ru>
2022-04-21 12:36:29 +0600

    add Primer001

---
+++ Primer001/Program.cs
@@ -0,0 +1,24 @@
+﻿int firstNumber;
+int secondNumber;
+Console.WriteLine("Напишите первое число: ");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число: ");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber)
+{
+    Console.WriteLine("Равные числа");
+}
+else
+{
+    if (firstNumber > secondNumber)
+    {
+        Console.Write(firstNumber);
+        Console.WriteLine("Первое число больше");
+    }
+    else
+    {
+        Console.Write(secondNumber);
+        Console.WriteLine("Второе число больше");
+    }
+}
