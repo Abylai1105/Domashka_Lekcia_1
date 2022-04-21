81717261a5d0c43e55aa8b3034b2996202fa025f
Abylai <aba.omar@mail.ru>
2022-04-21 19:08:05 +0600

    add Primer005

---
+++ Primer005/Program.cs
@@ -0,0 +1,6 @@
+﻿int number;
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+int lastdigit = (number % 10);
+Console.WriteLine("Посделняя цифра: ");
+Console.WriteLine(lastdigit);
