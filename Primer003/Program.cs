dfd8e74868b2d4f34370c8d1fe3048d6f4215948
Abylai <aba.omar@mail.ru>
2022-04-21 14:12:26 +0600

    add Primer003

---
+++ Primer003/Program.cs
@@ -0,0 +1,12 @@
+﻿int number;
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+int numberEven = number % 2;
+if( numberEven == 1 )
+{
+    Console.WriteLine("Не четное");
+}
+else
+{
+    Console.WriteLine("Четное");
+}
