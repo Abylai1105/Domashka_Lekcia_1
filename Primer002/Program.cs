e2291f96c8b7174da0d0c145107a50406663641d
Abylai <aba.omar@mail.ru>
2022-04-21 13:24:05 +0600

    add Primer002

--- Primer002/Program.cs
+++ Primer002/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1;
+int number2;
+int number3;
+Console.Write("Введите первое число: ");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+{
+    if(number1 == number3)
+    {
+        Console.WriteLine("Равные числа");
+    }
+    
+}
+else
+{
+    if(number1 > number2)
+    {
+        if(number1 > number3)
+        {
+            Console.Write(number1);
+            Console.WriteLine( "Первое больше" );
+        }
+        else
+        {
+            Console.Write(number3);
+            Console.WriteLine( "Третье больше" );
+        }
+    }
+    else
+    {
+        if(number2 > number3)
+        {
+            Console.Write(number2);
+            Console.WriteLine( "Второе больше" );
+        }
+        else
+        {
+            Console.Write(number3);
+            Console.WriteLine( "Третье больше" );
+        }
+    }
+}
